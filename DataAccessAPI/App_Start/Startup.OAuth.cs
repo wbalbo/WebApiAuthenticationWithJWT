88b18257badca9862ee4d3f64c814dfe11ce2017
welli <welli@DESKTOP-NRCD0LS>
2018-04-07 11:55:05 -0300

    Construção do projeto

---
+++ DataAccessAPI/App_Start/Startup.OAuth.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+using DataAccessAPI.Core;
+using DataAccessAPI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+namespace DataAccessAPI.App_Start
+{
+    public class Startup
+    {
+    }
+}
---
+++ DataAccessAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DataAccessAPI.Startup))]
+namespace DataAccessAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureOAuth(app);
+        }
+    }
+}
