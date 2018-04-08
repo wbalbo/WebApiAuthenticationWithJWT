b78bf342718b9fc868079517706a698e906eaa5d
welli <welli@DESKTOP-NRCD0LS>
2018-04-08 13:43:18 -0300

    Construção do projeto.

--- DataAccessAPI/App_Start/Startup.OAuth.cs
+++ DataAccessAPI/App_Start/Startup.OAuth.cs
@@ -1,19 +1,45 @@
-﻿using System;
-using System.Configuration;
-using DataAccessAPI.Core;
-using DataAccessAPI;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.Owin;
-using Microsoft.Owin.Security;
+﻿using System.Configuration;
+using Owin;
 using Microsoft.Owin.Security.DataHandler.Encoder;
+using DataAccessAPI.Core;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Jwt;
 using Microsoft.Owin.Security.OAuth;
-using Owin;
+using Microsoft.Owin;
+using System;
 
-namespace DataAccessAPI.App_Start
+namespace DataAccessAPI
 {
-    public class Startup
+    public partial class Startup
     {
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            var issuer = ConfigurationManager.AppSettings["issuer"];
+            var secret = TextEncodings.Base64Url.Decode(ConfigurationManager.AppSettings["secret"]);
+
+            //Adiciona objetos ao contexto OWIN
+            app.CreatePerOwinContext(() => new ProductsContext());
+            app.CreatePerOwinContext(() => new ProductUserManager());
+
+            //Habilita autenticação/autorização bearer
+            app.UseJwtBearerAuthentication(new Microsoft.Owin.Security.Jwt.JwtBearerAuthenticationOptions()
+            {
+                AuthenticationMode = AuthenticationMode.Active,
+                AllowedAudiences = new[] { "Any" },
+                IssuerSecurityKeyProviders = new IIssuerSecurityKeyProvider[]
+                {
+                    new SymmetricKeyIssuerSecurityKeyProvider(issuer, secret)
+                }           
+            });
+
+            app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/oauth2/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+                Provider = new CustomOAuthProvider(),
+                AccessTokenFormat = new CustomJwtFormat(issuer)
+            });
+        }
     }
 }
